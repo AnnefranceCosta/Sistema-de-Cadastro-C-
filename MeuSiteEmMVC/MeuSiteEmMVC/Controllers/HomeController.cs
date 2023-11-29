57337a7646d5be6088f27f30ac14137de255a1a4
AnnefranceCosta <annefrance.costa@gmail.com>
2023-11-29 09:22:23 -0300

    First Commit
    First Commit

---
+++ MeuSiteEmMVC/MeuSiteEmMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MeuSiteEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Annefrance Costa";
+            home.Email = "Annefrance.costa@gmail.com";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
